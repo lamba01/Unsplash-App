843504d0aea6e71fa377ba3d5942d1fbe1a9b8e9
lamba01 <jamesspaul987@gmail.com>
2024-03-26 20:28:41 +0100

    fully designed home page, yet to add functionality

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,15 +1,22 @@
 import React from 'react';
+import './home.css'
 import heroleft from '../../assets/hero-left.png';
 import heroright from '../../assets/hero-right.png';
+import searchicon from '../../assets/Search.svg'
 
 function Home() {
   return (
     <div className='hero-section'>
-        <img src={heroleft} alt="" />
+        <img src={heroleft} alt="" className='hero-left'/>
+        <div className="search-container">
         <h2>Search</h2>
         <p>Search high-resolution images from Unsplash</p>
+        <div className="input-container">
         <input type="text" placeholder='Enter your keywords...' />
-        <img src={heroright} alt="" />
+        <img src={searchicon} alt="" className='search-icon' />
+        </div>
+        </div>
+        <img src={heroright} alt=""  className='hero-right'/>
     </div>
   )
 }
