638095ac853435ec0cb2cdc2262710f2ee8db7d5
lamba01 <jamesspaul987@gmail.com>
2024-03-27 11:30:26 +0100

    made home page and navbar responsive across all devices

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,37 @@
 
-import './App.css';
+// import './App.css';
+// import Home from './pages/Home/Home';
+// import Navbar from './components/navigation/Navbar';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <Navbar />
+//       <Home />
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from './pages/Home/Home';
 import Navbar from './components/navigation/Navbar';
+// import Search from './Search';
 
 function App() {
   return (
-    <div className="App">
+    <Router>
       <Navbar />
-      <Home />
-    </div>
+      <Routes>
+      <Route path="/" element={<Home />} />
+        {/* <Route path="/search/:query" component={Search} /> */}
+      </Routes>
+        
+      
+    </Router>
   );
 }
 
